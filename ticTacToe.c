31075d414ba70376c31b3b728aa5981b2b9494ab
Matt Beall <mtbeall@gmail.com>
2017-06-19 08:28:20 -0600

    Work continued.

--- ticTacToe.c
+++ ticTacToe.c
@@ -12,18 +12,95 @@ SQUARE_X    = 1,
 SQUARE_O    = 2
 } Square;
 
+typedef enum
+{
+    STATUS_WIN,
+    STATUS_LOSS,
+    STATUS_NONE
+}
 
 char printBoard[100] = "\n\r_1_|_2_|_3_\n\r_4_|_5_|_6_\n\r 7 | 8 | 9\n\r";
 
-Square gameBoard[3][3];
 
 
 int boardMoveScore[3][3];
 
+typedef struct {
+    Square cells[3][3];
+}GameBoard;
+
+GameBoard gameBoard;
+
+
+void ind2RC(int index, int* row, int* col);
+
+void rc2Ind(int row, int col, int* index);
+
+int getCellMoveScore(int cellNumber, GameBoard boardState);
+
+
+void determineBoardMoveScores(Square perspective);
+
+void clearGameBoard();
+
+void printGameBoard();
+
+void getUserPlay(char* player, int* rowResult, int* colResult);
+
+int ticTacToe(bool userFirst);
+
+
+
+void ind2RC(int index, int* row, int* col)
+{
+    *row = (index - 1) / 3;
+    *col = index - (*row)*3 - 1;
+}
+
+void rc2Ind(int row, int col, int* index)
+{
+    *index = row*3 + col + 1;
+}
+
+int getCellMoveScore(int cellNumber, GameBoard boardState)
+{
+    GameBoard nextMoveGameBoard;
+    nextMoveGameBoard = boardState;
+    int cellValue;
+    cellValue = 0;
+
+    switch(getVictoryStatus(boardState))
+    {
+        case STATUS_WIN:
+            cellValue = 10;
+            return cellValue;
+            break;
+        case STATUS_LOSS:
+            cellValue = -10;
+            return cellValue;
+            break;
+        case STATUS_NONE:
+        default:
+            break;
+    }
+
+    int row, col;
+    for(row = 0; row < 3; row++)
+    {
+        for(col = 0; col < 3; col++)
+        {
+            //YOU ARE HERE!!!!!
+            cellValue += getCellMoveScore()
+        }
+    }
+    return cellValue;
+}
+
+
 void determineBoardMoveScores(Square perspective)
 {
     //Recursive Mumbo Jumbo to score each square based on decision tree and whether it's winning or losing
-    
+
 
 }
 
