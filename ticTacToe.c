8952abc7a5aad72aa9aab1ead1d2ad6dad16461a
Matt Beall <mtbeall@gmail.com>
2017-06-19 20:57:08 -0600

    Added makefile and some organization

--- ticTacToe.c
+++ ticTacToe.c
@@ -2,22 +2,9 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include "types.h"
+#include "gameState.h"
 
-
-
-typedef enum
-{
-SQUARE_NONE = 0,
-SQUARE_X    = 1,
-SQUARE_O    = 2
-} Square;
-
-typedef enum
-{
-    STATUS_WIN,
-    STATUS_LOSS,
-    STATUS_NONE
-}
 
 char printBoard[100] = "\n\r_1_|_2_|_3_\n\r_4_|_5_|_6_\n\r 7 | 8 | 9\n\r";
 
@@ -25,21 +12,18 @@ char printBoard[100] = "\n\r_1_|_2_|_3_\n\r_4_|_5_|_6_\n\r 7 | 8 | 9\n\r";
 
 int boardMoveScore[3][3];
 
-typedef struct {
-    Square cells[3][3];
-}GameBoard;
 
 GameBoard gameBoard;
 
 
-void ind2RC(int index, int* row, int* col);
+void ind2Sub(int index, int* row, int* col);
 
-void rc2Ind(int row, int col, int* index);
+void sub2Ind(int row, int col, int* index);
+
 
 int getCellMoveScore(int cellNumber, GameBoard boardState);
 
-
-void determineBoardMoveScores(Square perspective);
+void determineBoardMoveScores(Player perspective);
 
 void clearGameBoard();
 
@@ -50,39 +34,40 @@ void getUserPlay(char* player, int* rowResult, int* colResult);
 int ticTacToe(bool userFirst);
 
 
-
-void ind2RC(int index, int* row, int* col)
+void ind2Sub(int index, int* row, int* col)
 {
     *row = (index - 1) / 3;
     *col = index - (*row)*3 - 1;
 }
 
-void rc2Ind(int row, int col, int* index)
+void sub2Ind(int row, int col, int* index)
 {
     *index = row*3 + col + 1;
 }
 
-int getCellMoveScore(int cellNumber, GameBoard boardState)
+//Input: Current game board, proposed next move
+//Return: value of the proposed next move
+int getCellMoveScore(int proposedMove, GameBoard boardState)
 {
     GameBoard nextMoveGameBoard;
     nextMoveGameBoard = boardState;
     int cellValue;
     cellValue = 0;
 
-    switch(getVictoryStatus(boardState))
-    {
-        case STATUS_WIN:
-            cellValue = 10;
-            return cellValue;
-            break;
-        case STATUS_LOSS:
-            cellValue = -10;
-            return cellValue;
-            break;
-        case STATUS_NONE:
-        default:
-            break;
-    }
+    // switch(getVictoryStatus(boardState))
+    // {
+    //     case STATUS_WIN:
+    //         cellValue = 10;
+    //         return cellValue;
+    //         break;
+    //     case STATUS_LOSS:
+    //         cellValue = -10;
+    //         return cellValue;
+    //         break;
+    //     case STATUS_NONE:
+    //     default:
+    //         break;
+    // }
 
     int row, col;
     for(row = 0; row < 3; row++)
@@ -90,14 +75,14 @@ int getCellMoveScore(int cellNumber, GameBoard boardState)
         for(col = 0; col < 3; col++)
         {
             //YOU ARE HERE!!!!!
-            cellValue += getCellMoveScore()
+            // cellValue += getCellMoveScore()
         }
     }
     return cellValue;
 }
 
 
-void determineBoardMoveScores(Square perspective)
+void determineBoardMoveScores(Player perspective)
 {
     //Recursive Mumbo Jumbo to score each square based on decision tree and whether it's winning or losing
 
@@ -112,7 +97,7 @@ void clearGameBoard()
     {
         for(col = 0; col < 3; col++)
         {
-            gameBoard[row][col] = SQUARE_NONE;
+            gameBoard.cells[row][col] = PLAYER_NONE;
         }
     }
 }
@@ -127,15 +112,15 @@ void printGameBoard()
         for(col = 0; col < 3; col++)
         {
             char squareStateStr[100];
-            switch(gameBoard[row][col])
+            switch(gameBoard.cells[row][col])
             {
-                case SQUARE_X:
+                case PLAYER_X:
                     sprintf(squareStateStr, "X");
                     break;
-                case SQUARE_O:
+                case PLAYER_O:
                     sprintf(squareStateStr, "O");
                     break;
-                case SQUARE_NONE:
+                case PLAYER_NONE:
                 default:
                     sprintf(squareStateStr, "%d", (row*3 + col + 1));
                     break;
@@ -161,8 +146,7 @@ void getUserPlay(char* player, int* rowResult, int* colResult)
     printf("Player %s, Move: ", player);
     scanf("%d", &i);
 
-    row = (i - 1) / 3;
-    col = i - row*3 - 1;
+    ind2Sub(i, &row, &col);
 
     *rowResult = row;
     *colResult = col;
@@ -173,14 +157,23 @@ int ticTacToe(bool userFirst)
     int row, col;
     if(userFirst)
     {
-        char p[10];
-        sprintf(p, "X");   
-        getUserPlay(p, &row, &col);
-        // printf("%d %d \n\r", row, col);
+        int stat = getVictoryStatus(&gameBoard);
+        printf("Status: %d\n\r", stat);
 
-        gameBoard[row][col] = SQUARE_X;
+        while(getVictoryStatus(&gameBoard) == STATUS_NONE)
+        {
+            printf("HERE");
+            char p[10];
+            sprintf(p, "X");
+            getUserPlay(p, &row, &col);
+            // printf("%d %d \n\r", row, col);
 
-        printGameBoard();
+            gameBoard.cells[row][col] = PLAYER_X;
+
+            printGameBoard();
+            stat = getVictoryStatus(&gameBoard);
+            printf("Status: %d\n\r", stat);
+        }
     }else
     {
 
